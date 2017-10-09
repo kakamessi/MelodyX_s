3da21c4e1b6eedecea9648fe447dfae925243fc9
kaka <125921384@qq.com>
2017-10-09 18:14:10 +0800

    no message

---
+++ app/src/main/java/com/angelmusic/student/infobean/ActionBean.java
@@ -0,0 +1,19 @@
+package com.angelmusic.student.infobean;
+
+/**
+ * Created by DELL on 2017/5/17.
+ */
+
+public class ActionBean {
+
+    private String[] codes;
+
+    public String[] getCodes() {
+        return codes;
+    }
+
+    public void setCodes(String[] codes) {
+        this.codes = codes;
+    }
+
+}
